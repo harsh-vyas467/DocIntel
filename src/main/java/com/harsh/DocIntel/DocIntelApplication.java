d8576014b1f8a4d8bd43afe943450e5e88b826a3
Harsh Vyas <harsh.vyas467@gmail.com>
2025-08-12 15:36:01 +0530

    basic spring boot project

---
+++ src/main/java/com/harsh/DocIntel/DocIntelApplication.java
@@ -0,0 +1,13 @@
+package com.harsh.DocIntel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocIntelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocIntelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harsh/DocIntel/DocIntelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsh.DocIntel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocIntelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
